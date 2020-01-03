d8197cba0923b48c61805c34316dd15b7e0f416b
San Ayush <41052723+San1357@users.noreply.github.com>
2020-01-03 20:41:48 +0530

    simple Interest

---
+++ programme_to _find SImPLe_interest.py
@@ -0,0 +1,11 @@
+# S.I = (P * R * T )/100 
+
+
+p = int(input("Enter the Principle amount:"))
+r = int(input("Enter the rate:"))
+t = int(input("Enter the time:"))
+
+SI = (p*r*t)/100
+
+print ("The S.I is: ", SI)
+
