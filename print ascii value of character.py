a2b49f7fa2c9e77c2a82b093c9782017da96cf99
San Ayush <41052723+San1357@users.noreply.github.com>
2020-01-04 13:04:46 +0530

    Update print ascii value of character.py

--- print ascii value of character.py
+++ print ascii value of character.py
@@ -1,5 +1,7 @@
 # This Code will print Ascii Value of Character
 
+### ord()  : It coverts the given string of length one, return an integer representing the unicode code point of the character
+
 #CODE:
 
 a = input("enter the character:")
